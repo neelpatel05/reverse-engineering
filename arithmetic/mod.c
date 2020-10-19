d6d2709569d070dfd228f6b032a2091f1bc6ed6c
Neel Patel <neel.patel6573@gmail.com>
2020-10-18 23:00:29 -0400

    adds arithmetic

---
+++ arithmetic/mod.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+   int op1, op2, mod;      // variable declaration
+   
+   op1 = 6;                // variable definition
+   op2 = 3;
+   
+   mod = op1 % op2;            // division operation
+   
+   printf("Output of %d divide by %d is %d", op1, op2, mod);
+}
---
+++ arithmetic/mod.s
@@ -0,0 +1,85 @@
+	.file	"mod.c"
+	.text
+	.section	.rodata
+	.align 4
+.LC0:
+	.string	"Output of %d divide by %d is %d"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	endbr32
+	leal	4(%esp), %ecx
+	.cfi_def_cfa 1, 0
+	andl	$-16, %esp
+	pushl	-4(%ecx)
+	pushl	%ebp
+	.cfi_escape 0x10,0x5,0x2,0x75,0
+	movl	%esp, %ebp
+	pushl	%ebx
+	pushl	%ecx
+	.cfi_escape 0xf,0x3,0x75,0x78,0x6
+	.cfi_escape 0x10,0x3,0x2,0x75,0x7c
+	subl	$16, %esp
+	call	__x86.get_pc_thunk.cx
+	addl	$_GLOBAL_OFFSET_TABLE_, %ecx
+	movl	$6, -20(%ebp)
+	movl	$3, -16(%ebp)
+	movl	-20(%ebp), %eax
+	cltd
+	idivl	-16(%ebp)
+	movl	%edx, -12(%ebp)
+	pushl	-12(%ebp)
+	pushl	-16(%ebp)
+	pushl	-20(%ebp)
+	leal	.LC0@GOTOFF(%ecx), %eax
+	pushl	%eax
+	movl	%ecx, %ebx
+	call	printf@PLT
+	addl	$16, %esp
+	movl	$0, %eax
+	leal	-8(%ebp), %esp
+	popl	%ecx
+	.cfi_restore 1
+	.cfi_def_cfa 1, 0
+	popl	%ebx
+	.cfi_restore 3
+	popl	%ebp
+	.cfi_restore 5
+	leal	-4(%ecx), %esp
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.section	.text.__x86.get_pc_thunk.cx,"axG",@progbits,__x86.get_pc_thunk.cx,comdat
+	.globl	__x86.get_pc_thunk.cx
+	.hidden	__x86.get_pc_thunk.cx
+	.type	__x86.get_pc_thunk.cx, @function
+__x86.get_pc_thunk.cx:
+.LFB1:
+	.cfi_startproc
+	movl	(%esp), %ecx
+	ret
+	.cfi_endproc
+.LFE1:
+	.ident	"GCC: (Ubuntu 9.3.0-17ubuntu1~20.04) 9.3.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 4
+	.long	 1f - 0f
+	.long	 4f - 1f
+	.long	 5
+0:
+	.string	 "GNU"
+1:
+	.align 4
+	.long	 0xc0000002
+	.long	 3f - 2f
+2:
+	.long	 0x3
+3:
+	.align 4
+4:
